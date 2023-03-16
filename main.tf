896746043726e1716e326dadfd36aa89ca2c8df6
unknown <swarnavure@gmail.com>
2023-03-16 12:24:13 +0530

    keys disabled

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "aws" {
   region = var.region
+  /*
+   access_key = "AKIAYJ3X7MBFMQEEEURD"
+   secret_key =  "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"
+  */
 }
 
 data "aws_ami" "ubuntu" {
